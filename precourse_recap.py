77481114f149a52e4ae013bba2cc9164c4637fa6
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-02 16:09:42 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,15 @@
+word = "hello"
+num_1 = 8
+num_2 = 6
+var = False
+name = "Shuna"
+age = 31
+
+print(word)
+print(num_1 + num_2)
+
+if age > 18:
+    print ("Hello, " + name)
+else:
+    print("Goodbye, " + name)
+
