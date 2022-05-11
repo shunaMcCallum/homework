80da60e40b2768ebe3177da0f6e3dafb1c7ba36a
Jean BaMaung <34654518+jbamaung17@users.noreply.github.com>
2022-05-11 15:33:49 +0100

    removed advanced solution

--- week_02/day_3/testing_lab/advanced_solution/src/food.py
+++
@@ -1,6 +0,0 @@
-class Food:
-    
-    def __init__(self, name, price, rejuvination_level):
-        self.name = name
-        self.price = price
-        self.rejuvination_level = rejuvination_level
