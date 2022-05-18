ae4fa0231b778b2134a929c6ca321d1ccdd3799a
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-18 23:47:59 +0100

    week_3 day_3 homework

---
+++ week_3/day_3/homework/models/orders_class.py
@@ -0,0 +1,8 @@
+class Orders():
+
+    def __init__(self, customer_name, book_title, description, order_date, quantity):
+        self.customer_name = customer_name
+        self.book_title = book_title
+        self.description = description
+        self.order_date = order_date
+        self.quantity = quantity
