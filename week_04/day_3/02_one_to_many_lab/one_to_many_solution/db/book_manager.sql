589d6010930d1fc6504a5e460af59a7e26a2308d
Jean BaMaung <34654518+jbamaung17@users.noreply.github.com>
2022-05-26 11:26:17 +0100

    solutions for day 3 added

---
+++ week_04/day_3/02_one_to_many_lab/one_to_many_solution/db/book_manager.sql
@@ -0,0 +1,20 @@
+PRAGMA FOREIGN_KEYS = ON;
+
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR,
+  last_name VARCHAR
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR,
+  genre VARCHAR,
+  publisher VARCHAR,
+  author_id INTEGER NOT NULL,
+      FOREIGN KEY (author_id)
+         REFERENCES authors (id)
+);
