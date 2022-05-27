068c3645ec875519d446d9be2aa29ea6ecbb66a3
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-27 22:26:27 +0100

    8th commit - started member controller and HTML pages

--- week_4/day_5/python_project/gym_app/app.py
+++ week_4/day_5/python_project/gym_app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 
 # IMPORT CONTROLLERS HERE
+from controllers.member_controller import members_blueprint
 
 app = Flask(__name__)
 
 # REGISTER BLUEPRINTS HERE
+app.register_blueprint(members_blueprint)
 
-# ADD HOME ROUTE HERE
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
