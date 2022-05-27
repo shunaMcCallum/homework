2c580a0335ef1f21d5e3c679784e7d3c83a7bacb
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-27 14:40:54 +0100

    4th commit - basic structure added to loose files

---
+++ week_4/day_5/python_project/gym_app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# IMPORT CONTROLLERS HERE
+
+app = Flask(__name__)
+
+# REGISTER BLUEPRINTS HERE
+
+# ADD HOME ROUTE HERE
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
---
+++ week_4/day_5/python_project/gym_app/console.py
@@ -0,0 +1,10 @@
+import pdb
+from models.member import Member
+from models.session import Session
+from models.booking import Booking
+
+# IMPORT REPOSITORIES HERE
+
+# RUN DELETE METHODS HERE
+
+# CREATE DUMMY DATA AND TESTS HERE
