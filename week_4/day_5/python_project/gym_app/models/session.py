6048a2c8f369cb1961bf72f4cc1a1a34520c879c
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-27 14:28:26 +0100

    third commit - models

---
+++ week_4/day_5/python_project/gym_app/models/session.py
@@ -0,0 +1,9 @@
+class Session:
+
+    def __init__(self, name, date, description, duration, id = None):
+        self.name = name
+        self.date = date
+        self.description = description
+        self.duration = duration
+        self.id = id
+    
