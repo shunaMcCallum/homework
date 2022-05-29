76635e3567f76ad1625da3bdaf3729a058906c49
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-29 22:09:09 +0100

    14th commit - all html pages now done

--- week_4/day_5/python_project/gym_app/controllers/booking_controller.py
+++ week_4/day_5/python_project/gym_app/controllers/booking_controller.py
@@ -9,19 +9,22 @@ bookings_blueprint = Blueprint("bookings", __name__)
 # INDEX - SHOW ALL BOOKINGS
 @bookings_blueprint.route("/bookings")
 def bookings():
+    route_name = "bookings"
     bookings = booking_repository.select_all()
-    return render_template("bookings/index.html", bookings=bookings)
+    return render_template("bookings/index.html", route_name=route_name, bookings=bookings)
 
 # ADD NEW BOOKING
 @bookings_blueprint.route("/bookings/create")
 def add_booking():
+    route_name = "bookings"
     members = member_repository.select_all()
     workouts = workout_repository.select_all()
-    return render_template("bookings/create.html", members=members, workouts=workouts)
+    return render_template("bookings/create.html", route_name=route_name, members=members, workouts=workouts)
 
 # CREATE BOOKING
 @bookings_blueprint.route("/bookings", methods=['POST'])
 def create_booking():
+    route_name = "bookings"
     member_id = request.form['member_id']
     workout_id = request.form['workout_id']
 
@@ -30,10 +33,11 @@ def create_booking():
 
     booking = Booking(member, workout)
     booking_repository.save(booking)
-    return redirect("/bookings")
+    return redirect("/bookings", route_name=route_name)
 
 # DELETE BOOKING
 @bookings_blueprint.route("/bookings/<id>/delete", methods=['POST'])
 def delete_booking(id):
+    route_name = "bookings"
     booking_repository.delete(id)
-    return redirect("/bookings")
+    return redirect("/bookings", route_name=route_name)
--- week_4/day_5/python_project/gym_app/controllers/workout_controller.py
+++ week_4/day_5/python_project/gym_app/controllers/workout_controller.py
@@ -7,24 +7,28 @@ workouts_blueprint = Blueprint("workouts", __name__)
 # INDEX - SHOW ALL WORKOUTS
 @workouts_blueprint.route("/workouts")
 def workouts():
+    route_name = "workouts"
     workouts = workout_repository.select_all()
-    return render_template("workouts/index.html", workouts=workouts)
+    return render_template("workouts/index.html", route_name=route_name, workouts=workouts)
 
 # SHOW ONE WORKOUT
 @workouts_blueprint.route("/workouts/<id>")
 def workout_show(id):
+    route_name = "workouts"
     workout = workout_repository.select(id)
     members = workout_repository.get_members(workout)
-    return render_template("workouts/show.html", workout=workout, members=members)
+    return render_template("workouts/show.html", route_name=route_name, workout=workout, members=members)
 
 # ADD NEW WORKOUT
 @workouts_blueprint.route("/workouts/create")
 def create_workout():
-    return render_template("workouts/create.html")
+    route_name = "workouts"
+    return render_template("workouts/create.html", route_name=route_name)
 
 # CREATE WORKOUT
 @workouts_blueprint.route("/workouts", methods=['POST'])
 def add_workout():
+    route_name = "workouts"
     name = request.form['name']
     date = request.form['date']
     description = request.form['description']
@@ -32,17 +36,19 @@ def add_workout():
 
     workout = Workout(name, date, description, duration)
     workout_repository.save(workout)
-    return redirect("/workouts")
+    return redirect("/workouts", route_name=route_name)
 
 # EDIT WORKOUT
 @workouts_blueprint.route("/workouts/<id>/update")
 def edit_workout(id):
+    route_name = "workouts"
     workout = workout_repository.select(id)
-    return render_template("workouts/edit.html", workout=workout)
+    return render_template("workouts/edit.html", route_name=route_name, workout=workout)
 
 # UPDATE WORKOUT
 @workouts_blueprint.route("/workouts/<id>", methods=['POST'])
 def update_workout(id):
+    route_name = "workouts"
     name = request.form['name']
     date = request.form['date']
     description = request.form['description']
@@ -51,10 +57,11 @@ def update_workout(id):
     workout = Workout(name, date, description, duration, id)
     workout_repository.update(workout)
     members = workout_repository.get_members(workout)
-    return render_template("workouts/show.html", workout=workout, members=members)
+    return render_template("workouts/show.html", route_name=route_name, workout=workout, members=members)
 
 # DELETE WORKOUT
 @workouts_blueprint.route("/workouts/<id>/delete", methods=['POST'])
 def delete_workout(id):
+    route_name = "workouts"
     workout_repository.delete(id)
-    return redirect("/workouts")
+    return redirect("/workouts", route_name=route_name)
