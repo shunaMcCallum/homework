07b674e65848657037a28ca3a3dd8ad11f15473a
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-05-29 21:57:52 +0100

    13th commit - workout html pages done

--- week_4/day_5/python_project/gym_app/controllers/member_controller.py
+++ week_4/day_5/python_project/gym_app/controllers/member_controller.py
@@ -7,41 +7,47 @@ members_blueprint = Blueprint("members", __name__)
 #  INDEX - SHOW ALL MEMBERS
 @members_blueprint.route("/members")
 def members():
+    route_name = "members"
     members = member_repository.select_all()
-    return render_template("members/index.html", members=members)
+    return render_template("members/index.html", route_name=route_name, members=members)
 
 # SHOW ONE MEMBER
 @members_blueprint.route("/members/<id>")
 def member_show(id):
+    route_name = "members"
     member = member_repository.select(id)
     workouts = member_repository.get_workouts(member)
-    return render_template("members/show.html", member=member, workouts=workouts)
+    return render_template("members/show.html", route_name=route_name, member=member, workouts=workouts)
 
 # ADD NEW MEMBER
 @members_blueprint.route("/members/create")
 def add_member():
-    return render_template("members/create.html")
+    route_name = "members"
+    return render_template("members/create.html", route_name=route_name)
 
 # CREATE MEMBER
 @members_blueprint.route("/members", methods=['POST'])
 def create_member():
+    route_name = "members"
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     dob = request.form['dob']
 
     member = Member(first_name, last_name, dob)
     member_repository.save(member)
-    return redirect("/members")
+    return redirect("/members", route_name=route_name)
 
 # EDIT MEMBER
 @members_blueprint.route("/members/<id>/update")
 def edit_member(id):
+    route_name = "members"
     member = member_repository.select(id)
-    return render_template("/members/edit.html", member=member)
+    return render_template("/members/edit.html", route_name=route_name, member=member)
 
 # UPDATE MEMBER
 @members_blueprint.route("/members/<id>", methods=['POST'])
 def update_member(id):
+    route_name = "members"
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     dob = request.form['dob']
@@ -49,10 +55,11 @@ def update_member(id):
     member = Member(first_name, last_name, dob, id)
     member_repository.update(member)
     workouts = member_repository.get_workouts(member)
-    return render_template("members/show.html", member=member, workouts=workouts)
+    return render_template("members/show.html", route_name=route_name, member=member, workouts=workouts)
 
 # DELETE MEMBER
 @members_blueprint.route("/members/<id>/delete", methods=['POST'])
 def delete_member(id):
+    route_name = "members"
     member_repository.delete(id)
-    return redirect("/members")
+    return redirect("/members", route_name=route_name)
