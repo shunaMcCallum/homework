285c22ab2206bb910e5aa1741cf131cb1d04428c
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-06-13 23:14:13 +0100

    deleting duplicated files

--- week_3/day_3/homework/models/orders.py
+++
@@ -1,9 +0,0 @@
-from models.orders_class import *
-
-
-order_1 = Orders("John Smith", "Catcher in the Rye", "Classic Fiction book", "12/01/2022", 1)
-order_2 = Orders("Gary Johnson", "An Autobiography", "Autobiographical Non-fiction book", "28/03/2022", 2)
-order_3 = Orders("Samantha Banks", "The Girl on the Train", "Thriller Fiction book", "05/05/2022", 1)
-order_4 = Orders("Fiona Fiona", "Programming for Dummies", "Educaitonal Non-fiction book", "18/04/2022", 3)
-order_5 = Orders("Simon Green", "Winnie the Pooh", "Children's Fiction book", "15/05/2022", 1)
-orders = [order_1, order_2, order_3, order_4, order_5]
--- week_3/homework/models/players.py
+++
@@ -1,4 +0,0 @@
-from models.player import *
-
-player1 = Player("Player 1")
-player2 = Player("Player 2")
