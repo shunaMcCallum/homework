7b99ab86525d79016e094069bb43bcc361099bc3
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-06-14 22:35:54 +0100

    deleting files I accidentally downloaded

--- week_06/day_3/hw_traveller/hw_traveller_end/models/journey.js
+++
@@ -1,8 +0,0 @@
-const Journey = function(startLocation, endLocation, transport, distance) {
-  this.startLocation = startLocation;
-  this.endLocation = endLocation;
-  this.transport = transport;
-  this.distance = distance;
-};
-
-module.exports = Journey;
